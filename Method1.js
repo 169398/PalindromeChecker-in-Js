b946a1256d3ac86cac65e5f8ce104efa8a5ebf88
idris kulubi <137810838+169398@users.noreply.github.com>
2024-03-04 23:56:15 +0300

    Update Method1.js

--- Method1.js
+++ Method1.js
@@ -1,11 +1,11 @@
-// Define the function to check if a number is a palindrome
+// Define the function to check if a number is a palindrome or not
 function isPalindrome(num) {
     const strNum = num.toString();
     const reversedStrNum = strNum.split('').reverse().join('');
     return strNum === reversedStrNum;
 }
 
-// Test the function with a number
+// Test the function with a number that can be written in both ways .
 const number = 12321;
 if (isPalindrome(number)) {
     console.log(`${number} is a palindrome.`);
